85fe97821faa38460364b420ad587879ae260a1a
Mark Nelson <mjnelson90@gmail.com>
2017-10-01 17:28:54 +0100

    init commit

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -20,18 +20,65 @@ return console.log(err);
 console.log(`connected to mongodb`)
 
 // retuns a curor - pointer to the documents
-db.collection(`Todos`).find({_id:
-  new ObjectID(`59c11cbe3f70763a88bb2a5d`)
-}).toArray().then((arr)=>{
-console.log(JSON.stringify(arr,undefined,2));
+// db.collection(`Todos`).insertMany([{text: "walk the dog", completed: "true"},{text: "say hello", completed: "false"}]).then(e)=>{
 
-}).catch((e)=>{
-  console.log(e);
+
+// db.collection(`Users`).insertMany([{name: "Lior", age: "26", location: "London"},{name: "Nelson", age: "25", location: "HH"},{name: "Arron", age: "59", location: "Lancs"}])
+// .then((count)=>{
+//   console.log(count)
+// }).catch((e)=>{
+//     console.log(e)
+//
+// })
+
+
+
+
+db.collection(`Users`).find({name:"Mark"}).toArray().then((docs)=>{
+  console.log(JSON.stringify(docs, 2, undefined))
+
+}, (err)=>{
+  console.log(`unable to fetch to-dos`, err)
 })
 
 
 
 
-// db.close();
+
+
+
+// , function (err, r){
+//   console.log(r)
+// })
+
+
+db.collection(`Todos`).find().count().then((count)=>{
+  console.log(`To Dos count ${count}`)
+
+}, (err)=>{
+  console.log(`unable to fetch to-dos`, err)
+})
+
+
+
+// col.insertMany([{a:1}, {a:2}]).then(function(r) {
+//    test.equal(2, r.insertedCount);
+//    // Finish up test
+//    db.close();
+
+
+
+
+        // db.collection(`Todos`).find({}).toArray().then((arr)=>{
+        // console.log(JSON.stringify(arr,undefined,2));
+        //
+        // }).catch((e)=>{
+        //   console.log(e);
+        // })
+
+
+
+
+ db.close();
 
 });
