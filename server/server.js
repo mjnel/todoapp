36311a9469eff22bbac8cb4725e0dfca142b3914
Mark Nelson <mark_nelson@live.co.uk>
2017-11-05 21:49:06 +0000

    Refactored files and added express

--- server/server.js
+++ server/server.js
@@ -1,34 +1,27 @@
-const mongoose = require("mongoose");
+var express = require("express");
+var bodyParser = require ("body-parser")
+
+// object destructing - creating a local variable from the mongoose property on the object 
+var {mongoose}= require("./db/mongoose");
+var {Todo}= require("./models/todo");
+var {User}= require("./models/user");
+
+var app = express();
+
+//body parser takes JSON and converts to JavaScript. 
+//app.use = middleware. 
+app.use(bodyParser.json())
+// return value is a function which is given back to express. Send JSON to express. 
 
 
-mongoose.Promise = global.Promise;
-
-mongoose.connect("mongodb://localhost:27017/ToDoApp");
-
-var Todo = mongoose.model("Todo",{
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
+app.post(`/todos`, (req,res)=>{
+    console.log(req.body)
     
-    completed: {
-        type: Number
-    }
-})
+});
 
 
-var otherToDo = new Todo ({
-    
-    text: "this is a test",
-    completed: false,
-     
-})
 
-otherToDo.save().then((savedDoc)=>{
-    console.log(`saved todo`, savedDoc);
-    }).catch((e)=>{
-    console.log(`ERROR!`, e)
-  
+
+app.listen(3000, ()=>{
+    console.log(`up on port 3000`)
 })
