dd8730d4ff441594f141b73b16edacdfb89bc3a3
Mark Nelson <mark_nelson@live.co.uk>
2017-11-06 17:51:29 +0000

    testing working

--- server/server.js
+++ server/server.js
@@ -1,22 +1,34 @@
 var express = require("express");
 var bodyParser = require ("body-parser")
 
-// object destructing - creating a local variable from the mongoose property on the object 
+// object destructing - creating a local variable from the mongoose property on the object
 var {mongoose}= require("./db/mongoose");
 var {Todo}= require("./models/todo");
 var {User}= require("./models/user");
 
 var app = express();
 
-//body parser takes JSON and converts to JavaScript. 
-//app.use = middleware. 
+//body parser takes JSON and converts to JavaScript.
+//app.use = middleware.
 app.use(bodyParser.json())
-// return value is a function which is given back to express. Send JSON to express. 
+// return value is a function which is given back to express. Send JSON to express.
 
 
 app.post(`/todos`, (req,res)=>{
-    console.log(req.body)
-    
+    var todo = new Todo({
+		text : req.body.text
+	})
+
+  todo.save().then((doc)=>{
+    res.send(doc)
+  }).catch((e)=>{
+    res.status(400).send(e)
+  })
+
+
+
+
+
 });
 
 
@@ -25,3 +37,5 @@ app.post(`/todos`, (req,res)=>{
 app.listen(3000, ()=>{
     console.log(`up on port 3000`)
 })
+
+module.exports ={app}
